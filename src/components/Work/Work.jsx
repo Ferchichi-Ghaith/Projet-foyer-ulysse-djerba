c7519ae651aeb127e19876cccceea0d8d0bec1a1
Ferchichi Ghaith <72187168+Ferchichi-Ghaith@users.noreply.github.com>
2023-03-12 13:44:16 +0100

    2 up

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -11,7 +11,7 @@ const Work = () => {
     whileInView="show"
     viewport={{ once: false, amount: 0.25 }}
     className={`paddings ${css.wrapper}`}>
-    <a className="anchor" id="service&architecture"></a>
+    <a className="anchor" id="service&plan"></a>
 
       <div className={`innerWidth flexCenter ${css.container}`}>
         {/* heading */}
